04e45a7ab0a97352208844278eb889ed63a1e11f
Tai-Ning <winnie1533@gmail.com>
2024-11-06 03:26:29 +0800

    Modify config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,8 @@
 module.exports = function override(config, env) {
     // 添加對 node: 前綴的支持
-    config.resolve.alias['node:buffer'] = require.resolve('buffer/');
-
+    config.resolve.alias = {
+    ...config.resolve.alias,
+    'node:buffer': require.resolve('buffer/'),
+  }
     return config;
 };
