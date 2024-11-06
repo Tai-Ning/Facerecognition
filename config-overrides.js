acfadc78dc30fc0424d48924bc025cdf08a56e40
Tai-Ning <winnie1533@gmail.com>
2024-11-06 12:55:07 +0800

    Remove buffer

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-    // 添加對 node: 前綴的支持
-    config.resolve.alias = {
-    ...config.resolve.alias,
-    'node:buffer': require.resolve('buffer/'),
-  }
-    return config;
-};
